f35ff9699537fb45af3262d73ea503a826240f02
pepo <raczpepo@gmail.com>
2024-03-12 18:07:05 +0100

    adjusted gui to installer

--- src/net/jchad/installer/gui/GUI.java
+++ src/net/jchad/installer/gui/GUI.java
@@ -1,34 +1,57 @@
 package net.jchad.installer.gui;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+import net.jchad.installer.config.ConfigManager;
+
+import java.nio.file.Path;
 
 public class GUI extends Application {
+
+    private ConfigManager configManager = new ConfigManager();
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        primaryStage.setTitle("JChad Setup");
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
+        VBox vbox = new VBox(10);
+        vbox.setPadding(new Insets(10));
+
+        Label welcomeLabel = new Label("Welcome to the JChad setup!");
+        vbox.getChildren().add(welcomeLabel);
+
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        Button chooseDirectoryButton = new Button("Choose Installation Directory");
+        chooseDirectoryButton.setOnAction(e -> {
+            Path directory = directoryChooser.showDialog(primaryStage).toPath();
+            configManager.setDownloadPath(directory.toString());
         });
+        vbox.getChildren().add(chooseDirectoryButton);
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        ComboBox<String> softwareComboBox = new ComboBox<>();
+        softwareComboBox.getItems().addAll("Client and Server", "Server", "Client");
+        softwareComboBox.setPromptText("Select Software to Install");
+        softwareComboBox.setOnAction(e -> {
+            int selectedIndex = softwareComboBox.getSelectionModel().getSelectedIndex();
+            configManager.setSoftwareToInstall(selectedIndex);
+        });
+        vbox.getChildren().add(softwareComboBox);
+
+        Button installButton = new Button("Install");
+        installButton.setOnAction(e -> configManager.process());
+        vbox.getChildren().add(installButton);
+
+        Scene scene = new Scene(vbox, 400, 200);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
