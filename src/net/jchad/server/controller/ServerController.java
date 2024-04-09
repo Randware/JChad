6c42e9766fc763f95b323f6ee053d4dbfa5a435b
GHaxZ <ghax.z.yt@proton.me>
2024-04-09 19:06:03 +0200

    Added first structuring for MVC model in server, defined pattern for error handling.

---
+++ src/net/jchad/server/controller/ServerController.java
@@ -0,0 +1,17 @@
+package net.jchad.server.controller;
+
+import net.jchad.server.model.error.MessageHandler;
+import net.jchad.server.model.server.Server;
+
+// Used for controlling the server from the view
+public class ServerController {
+    private final Server server;
+
+    public ServerController(MessageHandler messageHandler) {
+        server = new Server(messageHandler);
+    }
+
+    public void startServer() {
+        server.runServer();
+    }
+}
