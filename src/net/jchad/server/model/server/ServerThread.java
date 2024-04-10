082d3310ffa133a3d0acd53b1ba55bf2f289b288
Dari-OS <darius.dev@proton.me>
2024-04-10 17:47:47 +0200

    Added more code to MainSocket and ServerThread

--- src/net/jchad/server/model/server/ServerThread.java
+++ src/net/jchad/server/model/server/ServerThread.java
@@ -16,12 +16,18 @@ public class ServerThread extends Thread{
     private final PrintWriter printWriter;
     private final BufferedReader bufferedReader;
     private final Socket socket;
+    private String remoteAddress = "Unknown"; //The ip address of the client
 
     public ServerThread(Socket socket, MessageHandler messageHandler) throws IOException {
-        if (messageHandler == null) throw new IllegalArgumentException("MessageHandler can't be null");
+        if (messageHandler == null) {throw new IllegalArgumentException("Could not connect to [%s]: The MessageHandler is null".formatted(remoteAddress));}
         this.messageHandler = messageHandler;
-        if (socket == null) messageHandler.handleError(new IllegalArgumentException("Socket can't be null"));
+        if (socket == null) {messageHandler.handleError(new IllegalArgumentException("Could not connect to [%s]: The Socket is null".formatted(remoteAddress)));  close();}
         this.socket = socket;
+        this.remoteAddress = socket.getRemoteSocketAddress().toString();
+        if (socket.getOutputStream() == null) {
+            messageHandler.handleError(new NullPointerException("Could not connect to [%s]: The OutputStream is null".formatted(remoteAddress)));
+            close();
+            }
         printWriter = new PrintWriter(new BufferedOutputStream(socket.getOutputStream()));
         bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     }
@@ -30,15 +36,16 @@ public class ServerThread extends Thread{
     public void run() {
         String message = "";
         try {
+            // If the client closes the connection the bufferedReader
             while ((message = bufferedReader.readLine()) != null) {
                 messageHandler.handleInfo("Message from " + socket + ": " + message);
                 printWriter.println("Message received");
                 printWriter.flush();
             }
-            if (socket.isClosed()) close();
         } catch (IOException  e) {
-           messageHandler.handleError(new UncheckedIOException("Connection couldn't be established with %s because of an unexpected error".formatted(socket.getRemoteSocketAddress()),e));
-           close();
+           messageHandler.handleError(new UncheckedIOException("Could not connect to [%s]: An unknown error occurred".formatted(remoteAddress),e));
+        } finally {
+            close();
         }
 
     }
@@ -47,7 +54,7 @@ public class ServerThread extends Thread{
 
     /**
      * Closes all fields
-     * @throws IOException If an I/O error occurs. Should be catched by the MainSocket
+     * @throws IOException If an I/O error occurs. Should be caught by the MainSocket
      */
     public void close() {
         try {
@@ -55,9 +62,10 @@ public class ServerThread extends Thread{
             if (printWriter != null) printWriter.close();
             if (bufferedReader != null) bufferedReader.close();
             if (socket != null) socket.close();
-            Thread.currentThread().interrupt();
         } catch (IOException e) {
-            messageHandler.handleError(new UncheckedIOException("An unexpected I/O-Exception occurred",e));
+            messageHandler.handleError(new UncheckedIOException("Error while closing connection to [%s]: An unknown error occurred".formatted(remoteAddress),e));
+        } finally {
+            Thread.currentThread().interrupt();
         }
     }
 
@@ -79,14 +87,15 @@ public class ServerThread extends Thread{
     }
 
 
-
     /**
      * This ensures that all operations done to the list get executed correctly.
-     * Without this methode problems could arise when multiple threads add or remove  themselves from the array list
-     * due to the nature of the ArrayList (Because it isn't concurrency safe!)
+     * Without this methode problems could arise when multiple threads add or remove themselves from the array list
+     * due to the nature of the ArrayList (Because it isn't concurrency safe.)
      * @param statement The statement that gets executed on the arraylist
+     * @return The return value of the operation
+     *
      */
-    public  static synchronized <Returns> Returns listOperation(Function<List<ServerThread>, Returns> statement) {
+    public  static synchronized <R> R listOperation(Function<List<ServerThread>, R> statement) {
         return statement.apply(serverThreadList);
     }
 }
