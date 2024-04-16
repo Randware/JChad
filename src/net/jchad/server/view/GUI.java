f766da5e4d8beb9a9fabe52b5d50a4e2370b981d
Schoergi2332 <SchoerghoferMs@outlook.com>
2024-04-16 15:28:11 +0200

    added a gui for the server console (not finished)

--- src/net/jchad/server/view/GUI.java
+++ src/net/jchad/server/view/GUI.java
@@ -1,22 +1,70 @@
 package net.jchad.server.view;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import net.jchad.server.controller.ServerController;
 import net.jchad.server.model.error.MessageHandler;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 
 // Responsible for displaying server output in GUI mode
-public class GUI implements MessageHandler {
+public class GUI extends Application implements MessageHandler {
     private ServerController server;
+    private TextArea logArea;
+    private TextField cmdField;
 
     public static void main(String[] args) {
-        new GUI().runGUI();
+        Application.launch(args);
     }
 
-    private void runGUI() {
-        server = new ServerController(this);
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println("Current working directory: " + System.getProperty("user.dir"));
+    server = new ServerController(this);
 
-        // Display GUI
+    logArea = new TextArea();
+    logArea.setEditable(false);
+
+        cmdField = new TextField();
+        cmdField.setPromptText("Enter command here...");
+
+        MenuBar menuBar = new MenuBar();
+        Menu menu = new Menu("View");
+        MenuItem smallFont = new MenuItem("Small Font");
+        MenuItem mediumFont = new MenuItem("Medium Font");
+        MenuItem largeFont = new MenuItem("Large Font");
+
+        smallFont.setOnAction(e -> changeFontSize(13));
+        mediumFont.setOnAction(e -> changeFontSize(17));
+        largeFont.setOnAction(e -> changeFontSize(21));
+
+        menu.getItems().addAll(smallFont, mediumFont, largeFont);
+        menuBar.getMenus().add(menu);
+
+        VBox vbox = new VBox(menuBar, logArea, cmdField);
+        vbox.setSpacing(10);
+        vbox.setPadding(new Insets(0, 10, 10, 10));
+        VBox.setVgrow(logArea, Priority.ALWAYS);
+        vbox.setMaxHeight(Double.MAX_VALUE);
+
+        vbox.setStyle("-fx-font-size: 13px;");
+        Scene scene = new Scene(vbox, 800, 600);
+        stage.setTitle("Server GUI");
+        stage.setScene(scene);
+        stage.show();
     }
 
+    private void changeFontSize(int size) {
+        logArea.setStyle("-fx-font-size: " + size + "px;");
+        cmdField.setStyle("-fx-font-size: " + size + "px;");
+    }
     @Override
     public void handleFatalError(Exception e) {
 
@@ -36,4 +84,6 @@ public class GUI implements MessageHandler {
     public void handleInfo(String info) {
 
     }
+
+
 }
