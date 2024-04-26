b7772c706e16db564964bf3b64e5eeba1ee5fdb5
Schoergi2332 <SchoerghoferMs@outlook.com>
2024-04-26 18:00:48 +0200

    added an eventlistener to change the fontsize with a keycombination

--- src/net/jchad/server/view/GUI.java
+++ src/net/jchad/server/view/GUI.java
@@ -4,6 +4,10 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
@@ -20,7 +24,12 @@ import javafx.scene.control.ScrollPane;
 import java.awt.*;
 import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
+import java.util.ArrayList;
 
+//TODO: 1. Zeilenumbruch bei logArea
+//TODO: 2. Copy pasta bei logArea
+//TODO: 3. Menubar font size change
+//TODO: 4. Font size beim ersten öffnen an bildschirm anpassen
 
 // Responsible for displaying server output in GUI mode
 public class GUI extends Application implements MessageHandler {
@@ -29,6 +38,9 @@ public class GUI extends Application implements MessageHandler {
     private TextField cmdField = new TextField();
     private double sizeValue= 13;
     private ScrollPane scrollPane = new ScrollPane();
+    private final KeyCombination crtlMinus = new KeyCodeCombination(KeyCode.MINUS, KeyCombination.CONTROL_DOWN);
+    private final KeyCombination crtlPlus = new KeyCodeCombination(KeyCode.PLUS, KeyCombination.CONTROL_DOWN);
+    private final KeyCombination crtlR = new KeyCodeCombination(KeyCode.R, KeyCombination.CONTROL_DOWN);
 
     //launch method
     public static void main(String[] args) {
@@ -57,7 +69,7 @@ public class GUI extends Application implements MessageHandler {
         fontsSubMenu.getItems().addAll(increaseFontSize, standardFontSize, decreaseFontSize);
 
         settingsMenu.getItems().add(fontsSubMenu);
-        
+
         menuBar.getMenus().add(settingsMenu);
 
         scrollPane.setContent(logArea);
@@ -75,6 +87,19 @@ public class GUI extends Application implements MessageHandler {
         VBox.setVgrow(logArea, Priority.ALWAYS);
         vbox.setMaxHeight(Double.MAX_VALUE);
         vbox.setStyle("-fx-font-size: 13px;");
+
+        vbox.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+            if (crtlPlus.match(event)) {
+                changeFontSize(2);
+                event.consume();
+            } else if (crtlMinus.match(event)) {
+                changeFontSize(-2);
+                event.consume();
+            } else if (crtlR.match(event)) {
+                standardFontSizeMethod();
+                event.consume();
+            }
+        });
 
         Scene scene = new Scene(vbox, 800, 600);
         stage.setTitle("Server GUI");
