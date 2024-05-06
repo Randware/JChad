fb171d30dbda6ee2fc4b23fc979d2dad62cf2eb9
dari-os <Darius.Seyed@proton.me>
2024-05-06 08:15:56 +0200

    Deleted my own code (to re add it later) because of a mistake I made where I commited all my code under a wrong name instead of my GitHub account.

--- src/net/jchad/shared/networking/packets/BannedPacket.java
+++
@@ -1,7 +0,0 @@
-package net.jchad.shared.networking.packets;
-
-public class BannedPacket extends DefaultPacket{
-    public BannedPacket() {
-        super(PacketType.BANNED, "YOU HAVE BEEN BANNED FROM THE SERVER!");
-    }
-}
--- src/net/jchad/shared/networking/packets/NotWhitelistedPacket.java
+++
@@ -1,7 +0,0 @@
-package net.jchad.shared.networking.packets;
-
-public class NotWhitelistedPacket extends DefaultPacket{
-    public NotWhitelistedPacket() {
-        super(PacketType.NOT_WHITELISTED, "You are not whitelisted on this server. Contact the server admin if you believe this is a mistake.");
-    }
-}
