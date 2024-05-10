ac3eb00c93c331402556f22eaa6d953ab88af8c7
GHaxZ <ghax.z.yt@proton.me>
2024-05-10 23:29:15 +0200

    Fixed issue where GUI wouldn't close when clicking X

--- src/net/jchad/server/view/GUI.java
+++ src/net/jchad/server/view/GUI.java
@@ -343,5 +343,6 @@ public class GUI extends Application implements MessageHandler {
     @Override
     public void stop() throws Exception {
         server.stopServer();
+        System.exit(0);
     }
 }
