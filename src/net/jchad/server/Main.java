a7aad259c90db5d3751f6b5d406f3c4739642b92
Dari-OS <darius.dev@proton.me>
2024-05-10 22:21:33 +0200

    Added GitHubCommand

--- src/net/jchad/server/Main.java
+++ src/net/jchad/server/Main.java
@@ -5,7 +5,7 @@ import net.jchad.server.view.cli.CLI;
 import net.jchad.server.view.GUI;
 
 public class Main {
-    private static final boolean cliMode = false;
+    private static final boolean cliMode = true;
 
     public static void main(String[] args) {
         if (args.length > 0 && args[0].equals("-cl") || cliMode) {
