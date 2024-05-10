59619b60c7e4affca4ad09577be05fc0c324dc6c
dari-os <Darius.Seyed@proton.me>
2024-05-10 09:51:35 +0200

    Made the IgnoreValidation.java annotation public

--- src/net/jchad/tests/shared/TestInvalidPacket.java
+++ src/net/jchad/tests/shared/TestInvalidPacket.java
@@ -1,9 +1,13 @@
 package net.jchad.tests.shared;
 
+import net.jchad.server.model.command.Command;
+import net.jchad.shared.networking.packets.IgnoreValidation;
 import net.jchad.shared.networking.packets.Packet;
 import net.jchad.shared.networking.packets.PacketType;
 
 public class TestInvalidPacket implements Packet {
     private final PacketType packet_type = PacketType.DISCOVERY;
+    @IgnoreValidation
+    private final Command command = null;
     private final int idk = 14;
 }
