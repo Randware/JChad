7de4a89cef2092c9fd96c78accdc55c022c3a00b
Dari-OS <darius.dev@proton.me>
2024-05-11 01:21:46 +0200

    Added the InvalidPacket.java. It gets sent to the client to indicate that the last sent packet was not valid.

---
+++ src/net/jchad/shared/networking/packets/InvalidPacket.java
@@ -0,0 +1,28 @@
+package net.jchad.shared.networking.packets;
+
+public class InvalidPacket implements Packet{
+    private final PacketType packet_type = PacketType.INVALID;
+
+
+    private final PacketType required_packet_type;
+
+    @IgnoreValidation
+    private final String message;
+
+    public InvalidPacket(PacketType required_packet_type, String message) {
+        this.required_packet_type = required_packet_type;
+        this.message = message;
+    }
+
+    public PacketType getPacket_type() {
+        return packet_type;
+    }
+
+    public PacketType getRequired_packet_type() {
+        return required_packet_type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
