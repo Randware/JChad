bd2fff265e41acee05295bb16df39b9b260d436b
dari-os <Darius.Seyed@proton.me>
2024-05-21 23:05:40 +0200

    Fixed wrong error message when client disconnects abruptly

---
+++ src/net/jchad/client/model/client/testing/ClientSampleCode.java
@@ -0,0 +1,51 @@
+package net.jchad.client.model.client.testing;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+// Temporary client used for testing
+public class ClientSampleCode {
+    public static void main(String[] args) {
+        try {
+            // Create a socket to connect to the server
+            Socket socket = new Socket("localhost", 13814);
+
+            // Obtain input and output streams
+            OutputStream outputStream = socket.getOutputStream();
+            InputStream inputStream = socket.getInputStream();
+
+            // Wrap the streams with readers and writers for easier communication
+            PrintWriter out = new PrintWriter(outputStream, true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+
+            // Start a thread for listening to server responses
+            Thread listenerThread = new Thread(() -> {
+                try {
+                    String response;
+                    while ((response = in.readLine()) != null) {
+                        System.out.println("Server response: " + response);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            listenerThread.start();
+
+            // Scanner for user input
+            Scanner scanner = new Scanner(System.in);
+            StringBuilder builder = new StringBuilder();
+            // Start sending user input to the server
+            while (true) {
+
+                out.println(scanner.nextLine());
+                out.flush();
+
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/net/jchad/tests/shared/Test.java
+++ src/net/jchad/tests/shared/Test.java
@@ -21,14 +21,9 @@ public class Test {
     public static void main(String[] args) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, ImpossibleConversionException, InvalidAlgorithmParameterException {
 
 
-        System.out.println(new ClientMessagePacket("Hello world", "test").toJSON());
-        System.out.println(new JoinChatRequestPacket("test").toJSON());
+        System.out.println(new PasswordResponsePacket(CrypterManager.hash("test")).toJSON());
         System.out.println(new UsernameClientPacket("Dari_OS").toJSON());
-        /*
-        {"packet_type":"CLIENT_MESSAGE","message":"Hello world","chat":"test"}
-        {"packet_type":"JOIN_CHAT_REQUEST","chat_name":"test"}
-        {"packet_type":"USERNAME_CLIENT","username":"Dari_OS"}
-         */
-        System.out.println(Packet.fromJSON("{\"packet_type\":\"CLIENT_MESSAGE\",\"message\":\"Hello world\",\"chat\":\"test\"}").getClass().getSimpleName());
+        System.out.println(new ClientMessagePacket("Hello lovely world", "test").toJSON());
+        System.out.println(new JoinChatRequestPacket("test").toJSON());
     }
 }
