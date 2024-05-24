ccdb6f66f8b82c5590902afedf7b4820d0f1954e
GHaxZ <ghax.z.yt@gmail.com>
2024-05-25 00:58:33 +0200

    did some things

--- src/net/jchad/server/Main.java
+++ src/net/jchad/server/Main.java
@@ -5,7 +5,7 @@ import net.jchad.server.view.CLI;
 import net.jchad.server.view.GUI;
 
 public class Main {
-    private static final boolean cliMode = true;
+    private static final boolean cliMode = false;
 
     public static void main(String[] args) {
         if (args.length > 0 && args[0].equals("-cl") || cliMode) {
