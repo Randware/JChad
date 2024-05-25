c1c36858d1a81bb1b86f86246d296339231095f2
Dari-OS <darius.dev@proton.me>
2024-05-25 16:39:45 +0200

    Deleted   dashboard. It was good while it lasted

--- src/net/jchad/server/Main.java
+++ src/net/jchad/server/Main.java
@@ -5,16 +5,13 @@ import net.jchad.server.view.CLI;
 import net.jchad.server.view.GUI;
 
 public class Main {
-    private static final boolean cliMode = true;
+    private static final boolean cliMode = false;
 
     public static void main(String[] args) {
         if (args.length > 0 && args[0].equals("-cl") || cliMode) {
             // Launch console interface
             CLI.main(args);
-        } /*else if(args.length > 0 && args[0].equals("-new")) {
-            // Launch JavaFX GUI interface
-            net.jchad.server.view.dashboard.GUI.main(args);
-        }*/ else {
+        } else {
             GUI.main(args);
         }
     }
