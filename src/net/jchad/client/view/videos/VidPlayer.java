5482be482bb756a1505f84ae4a66056eb6fa30ea
Schoergi2332 <SchoerghoferMs@outlook.com>
2024-06-03 00:11:07 +0200

    updated connectSavedConnections and updated monkerizz

--- src/net/jchad/client/view/videos/VidPlayer.java
+++ src/net/jchad/client/view/videos/VidPlayer.java
@@ -2,42 +2,25 @@ package net.jchad.client.view.videos;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
-import java.io.File;
-
 public class VidPlayer extends Application {
-    private MediaPlayer mediaPlayer;
-    private Media media;
-    private MediaView mediaView;
 
-    private File videoFile = new File("src/net/jchad/client/view/videos/monkerizz2.mp4");
-    private String mediaUrl = videoFile.toURI().toString();
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        WebView webView = new WebView();
+        WebEngine webEngine = webView.getEngine();
+        webEngine.load("https://www.youtube.com/embed/8Qc29mubbzk?autoplay=1");
 
-    public void start(Stage primaryStage) {
-        BorderPane borderPane = new BorderPane();
-        media = new Media(mediaUrl);
-        mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.setAutoPlay(true);
-        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-        mediaPlayer.setOnReady(primaryStage::sizeToScene);
-        mediaView = new MediaView(mediaPlayer);
-        mediaView.preserveRatioProperty().set(true);
-
-        borderPane.setCenter(mediaView);
-
-        Scene scene = new Scene(borderPane);
+        Scene scene = new Scene(webView, 800, 600);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("monke Rizz");
-        primaryStage.setResizable(false);
+        primaryStage.setTitle("YouTube Video Player");
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        Application.launch(args);
+        launch(args);
     }
 }
